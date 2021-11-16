1c459d5a95cc3ad7738aa7e45eb20ab0661d2e23
nickonyi <nickoluoch1130@gmail.com>
2021-11-16 14:29:05 +0300

    rock paper scissors

--- script.js
+++ script.js
@@ -27,3 +27,30 @@ function singleRound(playerSelection, computerSelection) {
         return "You lose!";
     }
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerSelection = "";
+    let computerSelection = "";
+    let result = "";
+    while (playerScore < 5 && computerScore < 5) {
+        playerSelection = prompt("rock,paper,scissor");
+        computerSelection = computerPlay();
+        result = singleRound(playerSelection, computerSelection);
+        if (result == "You win!") {
+            playerScore++;
+        } else if (result == "You lose!") {
+            computerScore++;
+        }
+
+        console.log("player:" + playerScore);
+        console.log("computer:" + computerScore);
+        console.log(result);
+    }
+    if (playerScore > computerScore) {
+        console.log("You have defeated the computer,you are the man!");
+    } else {
+        console.log("what have you done you loser!you have doomed us!")
+    }
+}
